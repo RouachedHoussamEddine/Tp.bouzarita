83c6c49bf169a8b8ef66b6f02b0b733bcb6690f5
RouachedHoussamEddine <84995938+RouachedHoussamEddine@users.noreply.github.com>
2021-06-05 08:56:55 +0100

    Add files via upload

---
+++ Front-End/App.js
@@ -0,0 +1,47 @@
+import React, {Component} from 'react';
+import {StyleSheet, Text, View,FlatList} from 'react-native';
+export default class App extends Component{
+
+  state ={
+    data:[]
+  }
+
+  fetchData= async()=>{
+    const response = await fetch('https://raw.githubusercontent.com/Hardeepcoder/fake_json/master/Users');
+    const users = await response.json();
+    this.setState({data: users});
+
+  }
+componentDidMount(){
+  this.fetchData();
+}
+  render() {
+    return (
+      <View >
+       <FlatList
+       data={this.state.data}
+       keyExtractor={(item,index) => index.toString()}
+       renderItem={({item}) =>
+
+       <View style={{backgroundColor:'#abc123',padding:10,margin:10}}>
+          <Text style={{color:'#fff', fontWeight:'bold'}}>{item.name}</Text>
+          <Text style={{color:'#fff'}}>{item.email}</Text>
+          <Text>City: {item.address.city}</Text>
+         </View>
+
+       }
+
+       />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+});
